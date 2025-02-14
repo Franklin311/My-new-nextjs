bd96d3727c12e9700ac0eeb654d16c0ac5317c1e
Franklin Antony <franklinleo311@gmail.com>
2025-02-14 17:26:56 +0530

    new commit 3

--- app/blog/error.tsx
+++ app/blog/error.tsx
@@ -1,8 +1,4 @@
 "use client";
 export default function Page() {
-  return (<div> error !!!!!!!!!!!!!!!! 
-    <p> commit 1</p>
-    <p> commit 2</p>
-    <p> commit 3</p>
-  </div>);
+  return (<div> error !!!!!!!!!!!!!!!!</div>);
 }
